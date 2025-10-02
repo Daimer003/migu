accf1ed7e037de4fd24d43d53034e3ddc982ccff
Daymer Perdomo <daimer003@gmail.com>
2025-10-02 14:11:30 -0500

    feat: main

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,6 @@
 import BannerMigu from '@/components/BannerMigu.vue';
 import FeeInstagram from '@/components/FeeInstagram.vue';
 import HeroHeader from '@/components/heroHeader/HeroHeader.vue';
-import Layout from '@/components/layout/Layout.vue';
 import ProductGrid from '@/components/productGrid/ProductGrid.vue';
 import WhatsappButton from '@/components/whatsappButton/WhatsappButton.vue';
 
